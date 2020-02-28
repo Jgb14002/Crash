097af90bec50d4018d27680b43a4c5df49582495
LoudPacks <jbassos@live.com>
2020-02-28 16:30:22 -0500

    Add logging and error handling

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const apiRouter = require('./api/routes/router');
 
+app.use(morgan('dev'));
 app.use('/v1', apiRouter);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
