5999220a31cb83b9c2f873653567832bda628b47
LoudPacks <jbassos@live.com>
2020-02-29 12:17:57 -0500

    Remove old js src files

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const config = require('./config/config');
-const app = require('./app');
-
-const port = process.env.PORT;
-const server = http.createServer(app);
-
-server.listen(port);
